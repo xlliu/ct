4b27c9e82b3a45d2862aae9c98904cf6c91ffb49
xlliu <liuxuelong@qq.com>
2015-09-07 17:40:49 +0800

    a

--- ct.py
+++ ct.py
@@ -1,6 +1,26 @@
+#!flask/bin/python
+#-*- coding: utf-8 -*-
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
 from flask import Flask,render_template
+from Dac.DataSource import controltower_database, controltower_database_read_1
 
 app = Flask(__name__)
+
+
+@app.teardown_request
+def _db_close(exc):
+    '''
+    关闭mysql权限
+    :param exc:
+    :return:
+    '''
+    if not controltower_database.is_closed():
+        controltower_database.close()
+    if not controltower_database_read_1.is_closed():
+        controltower_database_read_1.close()
 
 
 @app.route('/')
