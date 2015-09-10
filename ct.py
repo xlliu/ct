dd22a2286d34bc9be83910855e3d14539468376b
unknown <zhangjinglei@zhangjinglei-PC.com>
2015-09-10 15:49:58 +0800

    a

--- ct.py
+++ ct.py
@@ -1,4 +1,7 @@
 #-*- coding: utf-8 -*-
+import logging
+from logging.handlers import RotatingFileHandler
+import os
 import sys
 
 
@@ -28,7 +31,27 @@ def _db_close(exc):
         controltower_database_read_1.close()
 
 
+#########
+# 记录错误日志
+#########
+def get_server_path():
+    return os.path.split(os.path.realpath(__file__))[0]
+logpath = os.path.join(os.path.dirname(get_server_path()), '_corp_platform_logs')
+if not os.path.exists(logpath):
+    os.mkdir(logpath)
+    fp = open("error.log", 'w')
+    fp.close()
+errorlogpath = os.path.join(logpath, 'error.log')
+errlogFormat = logging.Formatter(
+    '%(process)d %(asctime)s %(levelname)s: %(message)s '
+    '[in %(pathname)s:%(lineno)d]'
+)
+import platform
 
+file_handler = RotatingFileHandler(errorlogpath, maxBytes=50000000, backupCount=5)
+file_handler.setFormatter(errlogFormat)
+file_handler.setLevel(logging.ERROR)
+app.logger.addHandler(file_handler)
 
 scheduler.reStart()
 
