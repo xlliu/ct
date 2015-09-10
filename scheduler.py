94e55b31c8fe2ea97a265026f5961a8607259521
unknown <zhangjinglei@zhangjinglei-PC.com>
2015-09-10 16:37:43 +0800

    a

--- scheduler.py
+++ scheduler.py
@@ -87,7 +87,7 @@ def addJob(item):
             # print(str(jobs))
             # print(jobs[str(item.id)].cron)
             # print(item.cron)
-            if (str(item.id) in jobs) and jobs[str(item.id)].cron.strip()!=item.cron.strip():
+            if (str(item.id) in jobs) and jobs[str(item.id)]["cron"]!=item.cron.strip():
                 jobs[str(item.id)]["cron"]=item.cron.strip()
                 scheduler.reschedule_job(str(item.id), trigger='cron', **getCron(item.cron))
                 print '已重新定时'
